594f886867681dd3027fe9c96ee37103417d54a1
Heroleo89 <petya96@mail.com>
2020-05-17 17:08:17 +0200

    Add build

---
+++ build/precache-manifest.14addba85641566e796d91f8d37946e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26866f2d35d95375f88130590ab3960",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2a9f02b1700da89a054",
+    "url": "./static/css/main.1858ee9c.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a9f02b1700da89a054",
+    "url": "./static/js/main.0c0d08d0.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
